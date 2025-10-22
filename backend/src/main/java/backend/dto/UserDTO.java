daeb2fe5d8269f5bc85fe88c93ecb82cf616eb14
jihyun0121 <gheun098@gmail.com>
2025-10-22 15:29:52 +0900

    json 변수명 호환 추가

--- backend/src/main/java/backend/dto/UserDTO.java
+++ backend/src/main/java/backend/dto/UserDTO.java
@@ -1,12 +1,20 @@
 package backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDTO {
+    
+    @JsonProperty("user_email")
     private String userEmail;
+    
+    @JsonProperty("user_password")
     private String userPassword;
+
+    @JsonProperty("user_name")
     private String userName;
 }
