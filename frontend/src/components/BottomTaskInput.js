71253f762f9fd7ef3c14cdfb8eb16e32e48adaa6
jihyun0121 <gheun098@gmail.com>
2025-11-05 15:48:53 +0900

    하단 입력창 css 수정

--- frontend/src/components/BottomTaskInput.js
+++ frontend/src/components/BottomTaskInput.js
@@ -20,51 +20,28 @@ export default function BottomTaskInput({ categories = [], onAddTask }) {
 
     return (
         <div className="bottom-input-wrapper">
-            <div className="task-item bottom-task-item">
-                <div className="task-content">
-                    <div className="task-left">
-                        <button className="task-check-btn" disabled>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-                                <circle cx="10" cy="10" r="10" fill="none" stroke="#777" strokeWidth="1" />
-                            </svg>
-                        </button>
-
-                        <input ref={bottomInputRef} type="text" value={bottomInput} onChange={(e) => setBottomInput(e.target.value)} onKeyDown={handleEnter} placeholder="할 일 적는 칸" className="task-text-input" />
-                    </div>
-
-                    <div
-                        className="task-right"
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            cursor: "pointer",
-                        }}
-                        onClick={() => setPopupOpen((prev) => !prev)}
-                    >
-                        <img src={TaskIcon} alt="task" style={{ width: "13px", marginRight: "6px" }} />
-                        <span
-                            style={{
-                                fontSize: "12px",
-                                color: "#595959",
-                                whiteSpace: "nowrap",
-                            }}
-                        >
-                            작업
-                        </span>
-                    </div>
+            <div className="bottom-content">
+                <div className="bottom-left">
+                    <button className="bottom-check-btn" disabled></button>
+                    <input className="bottom-text-input" ref={bottomInputRef} type="text" value={bottomInput} onChange={(e) => setBottomInput(e.target.value)} onKeyDown={handleEnter} placeholder="할 일 적는 칸" />
                 </div>
 
-                {popupOpen && (
-                    <CategoryPopup
-                        categories={categories}
-                        onSelect={(cat) => {
-                            console.log("선택한 카테고리:", cat.category_name);
-                            setPopupOpen(false);
-                        }}
-                        onClose={() => setPopupOpen(false)}
-                    />
-                )}
+                <button className="bottom-menu-btn" onClick={() => setPopupOpen((prev) => !prev)}>
+                    <img className="bottom-category-btn" src={TaskIcon} alt="task" />
+                    <span className="bottom-category-span">작업</span>
+                </button>
             </div>
+
+            {popupOpen && (
+                <CategoryPopup
+                    categories={categories}
+                    onSelect={(cat) => {
+                        console.log("선택한 카테고리:", cat.category_name);
+                        setPopupOpen(false);
+                    }}
+                    onClose={() => setPopupOpen(false)}
+                />
+            )}
         </div>
     );
 }
