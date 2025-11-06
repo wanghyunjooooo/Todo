00a099fed24abdfb12cf39f637695ffd56dc2b6f
jihyun0121 <gheun098@gmail.com>
2025-11-06 16:42:50 +0900

    카테고리 팝업 버튼 css 수정

--- frontend/src/components/CategoryManagePopup.js
+++ frontend/src/components/CategoryManagePopup.js
@@ -22,21 +22,13 @@ export default function CategoryManagePopup({ onClose, onEdit, onDelete }) {
         <div className="category-options-popup" ref={popupRef}>
             <button
                 onClick={() => {
-                    onEdit(); // 페이지 상단 카테고리 이름 수정 시작
-                    onClose(); // 팝업 닫기
+                    onEdit();
+                    onClose();
                 }}
             >
-                카테고리 수정
+                카테고리 수정하기
             </button>
-            <div
-                style={{
-                    width: "100%",
-                    height: "1px",
-                    backgroundColor: "#E0E0E0",
-                    margin: "4px 0",
-                }}
-            />
-            <button onClick={onDelete}>카테고리 삭제</button>
+            <button onClick={onDelete}>카테고리 삭제하기</button>
         </div>
     );
 }
