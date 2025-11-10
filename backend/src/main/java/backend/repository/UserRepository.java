44b6b93a66d28f2b8eb9007b69ffae4dadc9f2ca
jihyun0121 <gheun098@gmail.com>
2025-11-10 13:14:39 +0900

    UserRepository에 @Repository 어노테이션 추가

--- backend/src/main/java/backend/repository/UserRepository.java
+++ backend/src/main/java/backend/repository/UserRepository.java
@@ -2,8 +2,11 @@ package backend.repository;
 
 import backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User>findByEmail(String Email);
 }
